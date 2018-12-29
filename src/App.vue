29e55bd7ebbffe818f72b267d18b4a37dd94f884
oyinkan <skusman@student.lautech.edu.ng>
2018-12-29 10:02:24 +0100

    a new vue app

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Page1 msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Page1 from './components/page1.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Page1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/page1.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <router-link to="/page2">Go to questions' page</router-link>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page1',
+  data () {
+    return {
+      msg: 'Test your fit to work'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
